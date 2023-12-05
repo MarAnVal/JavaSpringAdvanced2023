30560741b4fe23ebabb06d7913e1e18465a7c11c
Martina Valchanova <140260318+MarAnVal@users.noreply.github.com>
2023-12-05 02:08:38 +0200

    Initial commit

--- aquagatedb/src/main/java/bg/softuni/aquagatedb/repository/PictureRepo.java
+++ aquagatedb/src/main/java/bg/softuni/aquagatedb/repository/PictureRepo.java
@@ -4,10 +4,7 @@ import bg.softuni.aquagatedb.model.entity.Picture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PictureRepo extends JpaRepository<Picture, Long> {
 
-    Optional<Picture> findByPictureUrl(String pictureUrl);
 }
