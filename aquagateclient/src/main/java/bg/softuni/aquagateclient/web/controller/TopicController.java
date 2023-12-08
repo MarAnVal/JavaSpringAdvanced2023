7bb917a4eb1615d8940f84cd9cb7ffd775a18e9b
Martina Valchanova <140260318+MarAnVal@users.noreply.github.com>
2023-12-08 15:53:31 +0200

    commit

--- aquagateclient/src/main/java/bg/softuni/aquagateclient/web/controller/TopicController.java
+++ aquagateclient/src/main/java/bg/softuni/aquagateclient/web/controller/TopicController.java
@@ -7,6 +7,7 @@ import bg.softuni.aquagateclient.web.interceptor.annotation.PageTitle;
 import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
